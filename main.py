5b157a6efce90e3c02ea9d2f0c1611478fdfbebf
Dan Trickey <dan@trickey.io>
2018-05-05 12:59:16 +0100

    Remove logging because ew and create game loop

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import pygame
-import logging
+from display.colours import *
+
+pygame.init()
+
+screen_width = 600
+screen_height = 400
+game_title = "Soton Analytica"
+
+pygame.display.set_caption(game_title)
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+clock = pygame.time.Clock()
+
+game_playing = True
 
 
-logging.basicConfig(filename='display.log', filemode='w', level=logging.DEBUG)
+while game_playing:
 
-logging.info("Starting Soton Analytica Display")
+    screen.fill(BACKGROUND)
+    pygame.display.flip()
+    clock.tick(15)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_playing = False
+
+pygame.quit()
