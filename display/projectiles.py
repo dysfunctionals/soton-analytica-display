f98f888bfa7c5963250b547b0eaf3b3a02c7ee8c
Kajetan Champlewski <contact@kajetan.ch>
2018-05-06 08:36:47 +0100

    Allow custom projectile starting positions.

--- display/projectiles.py
+++ display/projectiles.py
@@ -8,7 +8,7 @@ class Projectile(pygame.sprite.Sprite):
 
     pixel_scale = 5
 
-    def __init__(self, icon, velocity, oscillate_height = 0, oscillate_rate = 1):
+    def __init__(self, icon, velocity, oscillate_height = 0, oscillate_rate = 1, random_start = True, start_y = 0, start_x = 0):
 
         super().__init__()
 
@@ -16,13 +16,17 @@ class Projectile(pygame.sprite.Sprite):
         self.draw_image()
         self.rect = self.image.get_rect()
 
-        clearance = math.floor((screen_height-oscillate_height)/2)
-        self.rect.y = random.randrange(clearance, screen_height - clearance)
+        if random_start:
+            clearance = math.floor((screen_height - oscillate_height) / 2)
+            self.rect.y = random.randrange(clearance, screen_height - clearance)
 
-        if velocity < 0:
-            self.rect.x = screen_width
+            if velocity < 0:
+                self.rect.x = screen_width
+            else:
+                self.rect.x = 0
         else:
-            self.rect.x = 0
+            self.rect.y = start_y
+            self.rect.x = start_x
 
         self.velocity = velocity
         self.oscillate_height = oscillate_height
@@ -39,5 +43,5 @@ class Projectile(pygame.sprite.Sprite):
         if self.oscillate_height != 0:
             self.rect.y = self.default_y + math.floor(math.sin((self.rect.x * self.oscillate_rate / screen_width) * 2 * math.pi) * self.oscillate_height)
 
-        if self.x < -1000 or self.x > screen_width+1000:
+        if self.rect.x < -500 or self.rect.x > screen_width+500:
             self.kill()
